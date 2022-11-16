590d77c8e361a72e184d7ba07e6db6bf339e1ba1
Brian Tacderan <briantacderan@gmail.com>
2022-11-16 07:35:02 -0800

    Make a bunch of useless changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,16 @@
 class PagesController < ApplicationController
-  
   def welcome
     @projects = Project.all
     @contacts = Contact.all
     @skills = Skill.all
-    
-    @s3 = "https://tacderan-code.s3.us-west-1.amazonaws.com/images/"
-      
     @stones = Stone.all
 
-  end
-  
-  def about
-  end
-  
-  def contact
+    @s3 = "https://tacderan-code.s3.us-west-1.amazonaws.com/images/"
   end
 
+  def about
+  end
+
+  def contact
+  end
 end
