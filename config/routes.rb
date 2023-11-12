8748eb6ab93eaa9f0ba7747d1227f85cad825635
Brian Tacderan <briantacderan@gmail.com>
2023-11-12 11:09:12 -0800

    Add hoodcast page and edit the picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :pages
   root 'pages#home'
-  get 'projectA' => 'pages#fmfitness'
+  get 'fmfitness' => 'pages#fmfitness'
+  get 'hoodcast' => 'pages#hoodcast'
 end
